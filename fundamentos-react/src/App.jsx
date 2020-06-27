ec43f0a2c092b140b0a36f01db2873a480978ade
Rodolfo Santos <santoseuro2018@gmail.com>
2020-06-27 19:46:45 -0300

    [ADD] Trabalhando com comunicação direta entre componentes

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -10,10 +10,15 @@ import ListaAluno from './components/repeticao/ListaAlunos'
 import TabelaProdutos from './components/repeticao/TabelaProdutos'
 import ParOuImpar from "./components/condicional/ParOuImpar"
 import UsuarioInfo from "./components/condicional/UsuarioInfo"
+import DiretaPai from "./components/comunicacao/DiretaPai"
 
 export default () => (
     <div id="app" >
         <h1>Fundamentos React </h1>
+
+        <Card titulo="Comunicação direta">
+            <DiretaPai></DiretaPai>
+        </Card>
 
         <Card titulo="Condicional ">
             <UsuarioInfo usuario={{nome: 'Fernando'}} ></UsuarioInfo>
---
+++ fundamentos-react/src/components/comunicacao/DiretaPai.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import DiretaFilho from './DiretasFilho'
+
+
+export default props => {
+
+    return (
+        <div>
+            <DiretaFilho texto="Filho 1"  numero={20} bool={true}></DiretaFilho>
+        </div>
+    )
+
+}
+
+
+
